f1b63b771d8cf34f14ae9607e9c83d5c5e8398e1
Neoriya <alena.neori@gmail.com>
2024-06-05 18:04:26 +0300

    almost Done but not working

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,44 +1,46 @@
 // функції для HTTP-запитів.
 
+import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 //----замінити ключ на свій, коли вийде зареєструватися-------------
 const API_KEY = '43173775-fc7269b10cca3a5d436001063';
+const BASE_URL = 'https://pixabay.com/api/';
 
-export function fetchImages(userRequest) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: userRequest,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-
-  return fetch(`https://pixabay.com/api/?${params}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status); // Перевірка статусу відповіді сервера
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        //перевірка наявності зображень у відповіді
-        throw new Error(
-          'Sorry, there are no images matching <br>your search query.Please try again!'
-        ); // Генерується помилка, якщо список зображень порожній
-      }
-      return data.hits;
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error);
-      iziToast.error({
-        message: error.message,
-        backgroundColor: '#EF4040',
-        messageColor: '#FAFAFB',
-        position: 'topRight',
-      });
-      throw error;
+export async function showImages(userRequest, page = 1) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: userRequest,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 15,
+        page: page,
+      },
     });
+
+    if (response.status !== 200) {
+      throw new Error('Request failed with status: ' + response.status); // Перевірка статусу відповіді сервера
+    }
+
+    if (response.data.hits.length === 0) {
+      //перевірка наявності зображень у відповіді
+      throw new Error(
+        'Sorry, there are no images matching <br>your search query.Please try again!'
+      ); // Генерується помилка, якщо список зображень порожній
+    }
+    return response.data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    iziToast.error({
+      message: error.message,
+      backgroundColor: '#EF4040',
+      messageColor: '#FAFAFB',
+      position: 'topRight',
+    });
+    throw error;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -4,27 +4,34 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { fetchImages } from './js/pixabay-api.js';
+import { showImages } from './js/pixabay-api.js';
 import { createMarkup } from './js/render-functions.js';
 
 const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('input');
 const list = document.querySelector('ul');
 const loaders = document.getElementById('spinner');
+const loadMoreButton = document.getElementById('load-more');
 
-searchInput.addEventListener('input', handleInput);
+// searchInput.addEventListener('input', handleInput);
 searchForm.addEventListener('submit', handleSubmit);
+loadMoreButton.addEventListener('click', handleLoadMore);
 
-let userRequest; // Змінна для запиту користувача
+let userRequest = ''; // Змінна для запиту користувача
 let lightbox; // Змінна для SimpleLightbox
+let page = 1; // Поточна сторінка
 
-function handleInput(event) {
-  userRequest = event.target.value.trim().toLowerCase();
-}
+// function handleInput(event) {
+//   userRequest = event.target.value.trim().toLowerCase();
+// }
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
   list.innerHTML = ''; // Очищення списку перед новим пошуком
+  page = 1; // Скидання сторінки до 1
+  loadMoreButton.style.display = 'none'; // Ховає кнопку "Load more"
+
+  userRequest = searchInput.value.trim().toLowerCase(); // Оновлення userRequest
 
   if (!userRequest || userRequest.trim() === '') {
     iziToast.error({
@@ -40,28 +47,64 @@ function handleSubmit(event) {
 
   loaders.style.display = 'block'; // Показує спінер
 
-  fetchImages(userRequest)
-    .then(images => {
-      list.innerHTML = ''; // Очищення списку перед новим пошуком
-      list.insertAdjacentHTML('beforeend', createMarkup(images));
-      // Ініціалізація SimpleLightbox
-      if (!lightbox) {
-        lightbox = new SimpleLightbox('.gallery a', {
-          nav: true,
-          captions: true,
-          captionsData: 'alt',
-          captionDelay: 250,
-        });
-      } else {
-        lightbox.refresh(); // Оновлення SimpleLightbox
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error);
-    })
-    .finally(() => {
-      loaders.style.display = 'none'; // Приховує спінер
-      searchInput.value = ''; // Очищення поля вводу
-      userRequest = ''; // Обнулення змінної userRequest
-    });
+  try {
+    const { images, totalHits } = await showImages(userRequest, page);
+    list.innerHTML = ''; // Очищення списку перед новим пошуком
+    list.insertAdjacentHTML('beforeend', createMarkup(images));
+    // list.innerHTML = createMarkup(images);
+    if (images.length < 15 || list.children.length >= totalHits) {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        backgroundColor: '#5A5A5A',
+        messageColor: '#FAFAFB',
+        position: 'topRight',
+      }); // Додано повідомлення про кінець колекції
+      loadMoreButton.style.display = 'none'; // Ховати кнопку "Load more", якщо досягнуто кінець колекції
+    } else {
+      loadMoreButton.style.display = 'block'; // Показує кнопку "Load more", якщо ще є зображення для завантаження
+    }
+    // Ініціалізація SimpleLightbox
+    if (!lightbox) {
+      lightbox = new SimpleLightbox('.gallery a', {
+        nav: true,
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+    } else {
+      lightbox.refresh(); // Оновлення SimpleLightbox
+    }
+    if (images.length === 15) {
+      loadMoreButton.style.display = 'block';
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  } finally {
+    loaders.style.display = 'none'; // Приховує спінер
+    searchInput.value = ''; // Очищення поля вводу
+    userRequest = ''; // Обнулення змінної userRequest
+  }
+}
+
+async function handleLoadMore() {
+  page += 1;
+  loaders.style.display = 'block'; // Показує спінер
+  try {
+    const { images, totalHits } = await showImages(userRequest, page);
+    list.insertAdjacentHTML('beforeend', createMarkup(images));
+    lightbox.refresh();
+    if (images.length < 15 || list.children.length >= totalHits) {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        backgroundColor: '#5A5A5A',
+        messageColor: '#FAFAFB',
+        position: 'topRight',
+      }); // Додано повідомлення про кінець колекції
+      loadMoreButton.style.display = 'none'; // Ховати кнопку "Load more", якщо досягнуто кінець колекції
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  } finally {
+    loaders.style.display = 'none'; // Приховує спінер
+  }
 }
